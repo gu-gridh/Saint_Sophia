5dd7c44794a48fc814b40ef152f40c9276f53727
AramKarimi <aram.karimi@gu.se>
2024-09-03 17:02:05 +0200

    use extra parameter for big files

--- saintsophia-backend/saintsophia/abstract/models.py
+++ saintsophia-backend/saintsophia/abstract/models.py
@@ -87,13 +87,13 @@ def save_tiled_pyramid_tif(obj, path=IIIFFileStorage().location):
     obj_path =os.path.join(path, get_original_path(obj, obj.file.name))
 
     # Get the original image as a Pillow object before saving
-    image_object = Image.open(obj.file.open())
-    image = pyvips.Image.new_from_array(image_object)
+    # image_object = Image.open(obj.file.open())
+    # image = pyvips.Image.new_from_array(image_object)
  
-    # image = pyvips.Image.new_from_file(obj_path, access='sequential')
+    image = pyvips.Image.new_from_file(obj_path, access='sequential')
 
     # Create temporary file
-    image.tiffsave(tmp_path, **TIFF_KWARGS)
+    image.tiffsave(tmp_path, bigtiff=True, **TIFF_KWARGS)
 
     # Prepare saving the new IIIF file
     with open(tmp_path, 'rb') as f:
